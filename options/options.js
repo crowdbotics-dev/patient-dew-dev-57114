1de81f9295d3e38d092027840357949608c421ae
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 06:14:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "patient-dew-dev-57114",
+  url: "https://patient-dew-dev-57114.botics.co",
+  api: "https://patient-dew-dev-57114.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
